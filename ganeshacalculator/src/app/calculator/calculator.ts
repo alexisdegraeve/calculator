df005c0dc6983e1f3d596927e4eb23a3c94bc6c9
Alexis Degraeve <alexis.degraeve@hotmail.com>
2026-04-17 17:45:07 +0200

    change calculator style

--- ganeshacalculator/src/app/calculator/calculator.ts
+++ ganeshacalculator/src/app/calculator/calculator.ts
@@ -8,8 +8,8 @@ import { Component } from '@angular/core';
 })
 export class Calculator {
   boutons = [
-    7, 8, 9,'/',
-    4, 5, 6,'*',
+    7, 8, 9,'÷',
+    4, 5, 6,'x',
     1, 2, 3,'-',
     0, '.', '=','+'
   ];
