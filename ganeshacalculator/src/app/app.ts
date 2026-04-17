3009079b998157de5d131fca7e25679f6f5521d3
Alexis Degraeve <alexis.degraeve@hotmail.com>
2026-04-17 20:58:59 +0200

    update package json

--- ganeshacalculator/src/app/app.ts
+++ ganeshacalculator/src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Calculator } from './calculator/calculator';
 
 @Component({
   selector: 'app-root',
-  imports: [Calculator, RouterOutlet],
+  imports: [Calculator],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
